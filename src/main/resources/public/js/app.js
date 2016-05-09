b62abf3aaecac0af8dd71c8ff3d7f045b7ba01b9
AZG <ajeiedse@gmail.com>
2016-05-09 12:07:45 +0100

    Add products dynamically.

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,11 +1,29 @@
-var url = 'https://is-webserver.herokuapp.com/hello';
+var url = 'https://is-webserver.herokuapp.com';
 
 function updateHelloMessage() {
-    $.getJSON(url, function (response) {
+    $.getJSON(url + '/hello', function (response) {
         $('#hello-message').text(response.message);
     });
 }
 
+function updateProductList() {
+    $.getJSON(url + '/products', function (response) {
+        var i = 1;
+        $.each(response, function () {
+            var element = '<tr>' +
+                '<td>' + response[i].id + '</td>' +
+                '<td>' + response[i].name + '</td>' +
+                '<td>' + response[i].price + '</td>' +
+                '<td><img src="http://www.fndvisions.org/img/cutecat.jpg"></td>' +
+                '</tr>';
+            $('#product-list').append(element);
+            i++;
+        });
+    });
+}
+
+
 $(document).ready(function () {
     updateHelloMessage();
+    updateProductList();
 });
